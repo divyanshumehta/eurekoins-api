5e0d8bb1325640affb786caa5a604e518d151c0d
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-09-21 11:56:41 +0530

    [BUGFIX]user registration fixed

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,34 +2,45 @@ class Api::UsersController < Api::ApplicationController
 
 	def register
 		res = {}
-		token = params[:token]
 		user = User.new
-		user.name = params[:user][:name]
-		user.email = params[:user][:image]
-		user.token = params[token]
-		user.image = params[:user][:image]
-		if params[:user][:referred_invite_code]
+		user.name = params[:name]
+		user.email = params[:email]
+		user.token = params[:token]
+		user.image = params[:image]
+		if User.find_by_email(user.email)
+			res[:status] = "1"     	# Email already exists
+			render json: res
+			return
+		end
+		if User.find_by_token(user.token)
+			res[:status] = "2"		# Token already exists
+			render json: res
+			return
+		end
+		if params[:referred_invite_code] and !params[:referred_invite_code].blank?
 			t = Transaction.new
 			t.user = User.first
-			t.recevier = user.email
+			t.receiver = user.email
 			user.coins = 50
 			t.save
 		end
 		user.coins = 0
 		usr_code = ""
 		user.name.split(" ").each do |word|
-			usr = usr + word[0]
+			usr_code = usr_code + word[0]
 		end
-		user.invite_code = "AVSKR" + usr_code
-		unique_invite_code = User.find_by_invite_code(user.invite_code)
-		unless unique_invite_code
+		user.invite_code = "AVSKR" + usr_code.upcase
+		if User.find_by_invite_code(user.invite_code)
 			user.invite_code += User.last.id.to_s
 		end
-		user.save
-
-		# Response 0 means successfully registered user
-		res[:status] = "0"
-		render json: res
+		if user.save!
+			# Response 0 means successfully registered user
+			res[:status] = "0"
+			render json: res
+		else
+			res[:status] = "3"
+			render json: res
+		end
 	end
 
 	# Returns ture or false if user with a particular token exists
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api , defaults: { format: 'json' } do
-    post '/register/:token' => 'users#register'
+    get '/register/:token' => 'users#register'
     get '/exists/:token' => 'users#exists'
     get '/coins/:token' => 'users#coins'
     get '/users/:pattern' => 'users#user_list'
